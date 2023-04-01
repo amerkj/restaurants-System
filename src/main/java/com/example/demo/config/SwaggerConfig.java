8b668571827bfaa6938f2e887a7717f8b82d359b
amerkj <amerkhadamaljamy@gmail.com>
2023-04-02 03:28:20 +0400

    swagger done on the port https://localhost:8443/swagger-ui/index.html#/

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -10,32 +10,36 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static springfox.documentation.builders.PathSelectors.regex;
-
-@EnableSwagger2
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
-
     @Bean
-    public Docket productApi() {
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
-                .paths(regex("/rest.*"))
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
                 .build();
     }
 
-
 }
+
+//
+//@EnableSwagger2
+//@Configuration
+//public class SwaggerConfig {
+//
+//    @Bean
+//    public Docket productApi() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
+//                .paths(regex("/.*"))
+//                .build();
+//    }
+//
+//
+//}
 
 //.apis(RequestHandlerSelectors.basePackage("com.example.demo.advertisement.AdvertisementController"))
 //                .apis(RequestHandlerSelectors.basePackage("com.example.demo.cuisinetype"))
